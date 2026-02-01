6146176efda6ab32625efc3c7e46d01b3426880c
Carolina Contreras Issa <AGirlNamedCaro@gmail.com>
2026-02-01 12:48:13 -0700

    removed spatial ref sys from schema

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module TerrafreightLite
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    ActiveRecord::SchemaDumper.ignore_tables += ["spatial_ref_sys", "geometry_columns", "spatial_database_template"]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- db/schema.rb
+++ db/schema.rb
@@ -24,12 +24,4 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_23_163119) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "spatial_ref_sys", primary_key: "srid", id: :integer, default: nil, force: :cascade do |t|
-    t.string "auth_name", limit: 256
-    t.integer "auth_srid"
-    t.string "srtext", limit: 2048
-    t.string "proj4text", limit: 2048
-    t.check_constraint "srid > 0 AND srid <= 998999", name: "spatial_ref_sys_srid_check"
-  end
 end
